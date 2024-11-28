9cff306a665d37caf48d1f6556d14de3399987e7
Langdon <getpacman.gq@gmail.com>
2024-11-28 12:17:46 -0500

    Add files

---
+++ src/Logger.java
@@ -0,0 +1,5 @@
+class Logger {
+    static void log (Exception e, String msg, int severity){
+
+    }
+}
---
+++ src/Settings.java
@@ -0,0 +1,7 @@
+public class Settings {
+    static boolean autoUpdate = true;
+    static boolean playPauseBeat = true;
+    static boolean mute = false;
+    static boolean debug = true;
+    static int scale = 1;
+}
