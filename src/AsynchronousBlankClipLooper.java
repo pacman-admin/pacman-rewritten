feaf1e271d821aeac6f4d7fdbf2f35ad5afa7067
langdon <getpacman.gq@gmail.com>
2025-05-02 18:58:02 -0400

    Tweaked rounding error compensation offset.

--- src/AsynchronousBlankClipLooper.java
+++ src/AsynchronousBlankClipLooper.java
@@ -7,13 +7,11 @@ import java.io.InputStream;
 
 final class AsynchronousBlankClipLooper implements Runnable {
     public void run() {
-
         try (Clip clip = AudioSystem.getClip(); InputStream is = AsynchronousBlankClipLooper.class.getResourceAsStream("DEATH.wav")) {
             assert is != null;
             clip.open(AudioSystem.getAudioInputStream(is));
             clip.loop(-1);
         } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-            clip.close();
             throw new RuntimeException(e);
         }
     }
